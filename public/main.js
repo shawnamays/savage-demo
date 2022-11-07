385b336d13ff9a9cbefb6a5e57b02fc8202c13ad
Shawna Mays <shawnadmays@gmail.com>
2022-11-07 00:34:03 -0500

    this is my routine personal express app

--- public/main.js
+++ public/main.js
@@ -1,74 +1,81 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var thumbDown = document.getElementsByClassName("fa-thumbs-down");
-var trash = document.getElementsByClassName("fa-trash-o");
-
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
-});
-
-// FETCH FOR THUMBS DOWN GOES HERE TO LOCATE IT----------------------------------------
-Array.from(thumbDown).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        // check the location that this directs to-------------------
-        const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbDown - 2
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
-});
-
-
+const trash = document.getElementsByClassName("fa-trash-o");
 
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+  element.addEventListener('click', function() {
+    const task = this.parentNode.parentNode.childNodes[1].innerText
+    const day = this.parentNode.parentNode.childNodes[3].innerText
+
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'task': task,
+        'day': day
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
 });
+
+
+
+//I'm going to get the sun sign by checking the value
+// of the date entered by the user
+
+
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+// // FETCH FOR THUMBS DOWN GOES HERE TO LOCATE IT----------------------------------------
+// Array.from(thumbDown).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         // check the location that this directs to-------------------
+//         const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbDown - 2
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+
+
--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+
+// my goal is for user to input their date and they are given their signs based on the data from
+//the API.  they can then input it and ????
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { request } = require('express');
 const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 //URL from mongodb website cluster
-const url = "mongodb+srv://geminimoonchild:1234@cluster0.o3h3upz.mongodb.net/savageProject?retryWrites=true&w=majority";
-const dbName = "savageProject";
+const url = "mongodb+srv://geminimoonchild:1234@cluster0.o3h3upz.mongodb.net/personalExpress?retryWrites=true&w=majority";
+const dbName = "personalExpress";
 
 app.listen(8080, () => {
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      throw error;
+    }
+    db = client.db(dbName);
+    console.log("Connected to `" + dbName + "`!");
+  });
+
+  
+//MAKE SURE TO CHANGE THIS TO MISTYFOXSONG AND THE HOROSCOPE PROJECT
+const url = "mongodb+srv://mistyfoxsong:1234@cluster0.ndqi8wd.mongodb.net/Horoscope?retryWrites=true&w=majority";
+const dbName = "Horoscope";
+
+app.listen(3000, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
@@ -16,63 +34,82 @@ app.listen(8080, () => {
         db = client.db(dbName);
         console.log("Connected to `" + dbName + "`!");
     });
+
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+
+
+
+
+
+
+
+//this request is making a READ/GET request from the root and asking it to retrieve
+//everything it knows about the request and response.
+// it go into the database, into the collection called "messages" and find the data
+//turn the data into an Array. If there's an error, let us know, if not, render the index.ejs
+//page and display the object (document) from our database from messages collection and the result data
+
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('birthChart').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', { messages: result })
   })
 })
+//THIS WAS CHANGED TO birthChart
+app.post('/birthChart', (req, res) => {
+  function check() {
 
+//this is to insert a new task from the user
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
+  let day = req.body.day
+  
+  //pre conversion checks to see what the typeof Day is
+  if (typeof day === 'string' /* This means only ONE day of the week was sent */) {
+    day = [day] /* We change it to an array that it is easier for Mongo to work with */
+  }
+
+  db.collection('messages').insertOne({ task: req.body.task, day: day }, 
+    (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/')
+    })
 })
 
+
+//this is to update the tasks
 app.put('/messages', (req, res) => {
   db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ task: req.body.task }, {
+      $set: {
+        
+      }
+    }, {
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+
+    })
 })
 
-//logic for thumbs down goes here =================
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      
-      thumbDown:req.body.thumbDown - 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+//I need to find a way to select the day so that the task is sorted into the chosen day
+
+
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  console.log("Info Being sent from the Front End: ", req.body)
+
+  const day = req.body.day.split(",")
+  console.log("Array Conversion: ", day)
+  
+  db.collection('messages').findOneAndDelete({ task: req.body.task, day: day }, (err, result) => {
     if (err) return res.send(500, err)
     res.send('Message deleted!')
   })
-})
